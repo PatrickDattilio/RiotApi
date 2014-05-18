ddfcac32bbb160edcc1bbca8f90f1a7e1649999a
Alex <Alex.Dattilio@gmail.com>
2014-05-18 01:35:21 -0400

    Added stats class and new fields (includes Stats stats, String partype) in Champion class

--- RiotApi/src/main/java/com/howbig/riot/type/Champion.java
+++ RiotApi/src/main/java/com/howbig/riot/type/Champion.java
@@ -14,6 +14,7 @@ public class Champion {
     public String[] allytips;
     public String[] enemytips;
     public String[] tags;
-   
+    public String partype;
+    public Stats stats;
 
 }
---
+++ RiotApi/src/main/java/com/howbig/riot/type/Stats.java
@@ -0,0 +1,27 @@
+package com.howbig.riot.type;
+
+/**
+ * Created by Alex on 5/18/2014.
+ */
+public class Stats {
+    public int hp;
+    public int hpperlevel;
+    public int mp;
+    public int mpperlevel;
+    public int movespeed;
+    public int armor;
+    public int armorperlevel;
+    public int spellblock;
+    public int spellblockperlevel;
+    public int attackrange;
+    public int hpregen;
+    public int hpregenperlevel;
+    public int mpregen;
+    public int mpregenperlevel;
+    public int crit;
+    public int critperlevel;
+    public int attackdamage;
+    public int attackdamageperlevel;
+    public int attackspeedoffset;
+    public int attackspeedperlevel;
+}
